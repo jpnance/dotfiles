69d520796881b34115008cddb9f67626accd6d74
Patrick Nance <jpnance@gmail.com>
2025-11-20 23:22:53 -0800

    I think this is dead but I might as well commit.

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-dotfiles=(vimrc vim spectrwm.conf spectrwm gitconfig tmux.conf)
+dotfiles=(vimrc vim spectrwm.conf spectrwm gitconfig tmux.conf Xresources xinitrc)
 
 for dotfile in ${dotfiles[*]}
 do
